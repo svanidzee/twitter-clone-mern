ca3ffb0ae0f10f8ea393e9a787affb43b8a7c32e
Irakli Svanidze <svanidzeee@gmail.com>
2023-06-22 14:03:43 +0300

    add user bio, user image, upload user image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import type { AppProps } from 'next/app';
 
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Toaster />
       <LoginModal />
       <RegisterModal />
+      <EditModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
